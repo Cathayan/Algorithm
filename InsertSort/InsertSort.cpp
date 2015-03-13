fcf20f97e3ea7745fffd80817bedbbd3febfd17c
zzhang02 <zzhang02@imaginecommunications.com>
2015-03-13 18:27:14 +0800

    add insert sort

---
+++ InsertSort/InsertSort.cpp
@@ -0,0 +1,54 @@
+// InsertSort.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+struct LIST
+{
+	struct LIST* pNext;
+	int iValue;
+};
+
+struct LIST* InsertSort(struct LIST* pList)
+{
+	if ((pList == NULL) || (pList->pNext == NULL))
+	{
+		return pList;
+	}
+
+	struct LIST* pHead = NULL, *pNoSortItor = pList;
+
+	while (pNoSortItor != NULL)
+	{
+		pNoSortItor = pList;
+		pList = pList->pNext;
+		if ((pHead == NULL) || (pNoSortItor->iValue < pHead->iValue))
+		{
+			pNoSortItor->pNext = pHead;
+			pHead = pNoSortItor;
+		}
+		else
+		{
+			struct LIST* pSortItor = pHead;
+			while (pSortItor != NULL)
+			{
+				if ((pSortItor->pNext == NULL) ||
+					(pNoSortItor->iValue < pSortItor->pNext->iValue))
+				{
+					pNoSortItor->pNext = pSortItor->pNext;
+					pSortItor->pNext = pNoSortItor;
+					break;
+				}
+				pSortItor = pSortItor->pNext;
+			}
+		}
+	}
+	return pHead;
+};
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
